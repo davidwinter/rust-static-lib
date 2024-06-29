8ee740e36923fc1f953f1fab286372e98e846d7c
David Winter <i@djw.me>
2024-06-29 19:44:10 +0100

    fix: add c++ dylib to rust build script

--- rust-app/build.rs
+++ rust-app/build.rs
@@ -1,4 +1,5 @@
 fn main() {
     println!("cargo:rustc-link-search=native=../cpp-lib/build/lib");
     println!("cargo:rustc-link-lib=static=greeting");
+    println!("cargo:rustc-flags=-l dylib=c++");
 }
