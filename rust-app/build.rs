d10d5677ea42f442220c051da071d63792064070
David Winter <i@djw.me>
2024-06-29 19:53:02 +0100

    fix: windows might not need c++ explicitly

--- rust-app/build.rs
+++ rust-app/build.rs
@@ -1,9 +1,7 @@
 fn main() {
     println!("cargo:rustc-link-search=native=../cpp-lib/build/lib");
     println!("cargo:rustc-link-lib=static=greeting");
-    if cfg!(target_os = "windows") {
-        println!("cargo:rustc-flags=-l dylib=msvcp");
-    } else if cfg!(target_os = "macos") {
+    if cfg!(target_os = "macos") {
         println!("cargo:rustc-flags=-l dylib=c++");
     } else if cfg!(target_os = "linux") {
         println!("cargo:rustc-flags=-l dylib=stdc++");
